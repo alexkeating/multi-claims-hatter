479b77a04e751320cb956d09c7df191917220e6c
spengrah <code@spengrah.xyz>
2024-05-28 16:06:30 -0500

    deploy v0.2.0

--- script/MultiClaimsHatter.s.sol
+++ script/MultiClaimsHatter.s.sol
@@ -3,6 +3,52 @@ pragma solidity ^0.8.19;
 
 import { Script, console2 } from "forge-std/Script.sol";
 import { MultiClaimsHatter } from "../src/MultiClaimsHatter.sol";
+import { HatsModuleFactory } from "hats-module/HatsModuleFactory.sol";
+
+contract DeployInstance is Script {
+  address public implementation = 0xB985eA1be961f7c4A4C45504444C02c88c4fdEF9;
+  address public instance;
+  HatsModuleFactory public factory = HatsModuleFactory(0xfE661c01891172046feE16D3a57c3Cf456729efA);
+  bytes32 public SALT = bytes32(abi.encode(0x4a75));
+  uint256 public SALT_NONCE = 1;
+
+  /// @dev Set up the deployer via their private key from the environment
+  function deployer() public returns (address) {
+    uint256 privKey = vm.envUint("PRIVATE_KEY");
+    return vm.rememberKey(privKey);
+  }
+
+  function createInitData() public pure returns (bytes memory) {
+    uint256 claimableHat1 = 26_960_769_425_706_497_914_046_077_453_500_346_168_786_499_100_899_720_886_694_455_541_760;
+    uint256 claimableHat2 = 26_960_769_425_706_689_475_988_685_689_607_640_962_164_892_889_547_673_229_084_728_492_032;
+    uint256[] memory hatIds = new uint256[](2);
+    hatIds[0] = claimableHat1;
+    hatIds[1] = claimableHat2;
+
+    MultiClaimsHatter.ClaimType claimType = MultiClaimsHatter.ClaimType.Claimable;
+    MultiClaimsHatter.ClaimType[] memory claimTypes = new MultiClaimsHatter.ClaimType[](2);
+    claimTypes[0] = claimType;
+    claimTypes[1] = claimType;
+
+    return abi.encode(hatIds, claimTypes);
+  }
+
+  function run() public virtual {
+    vm.startBroadcast(deployer());
+
+    instance = factory.createHatsModule(
+      implementation,
+      26_960_769_425_706_402_133_074_773_335_446_698_772_097_302_206_575_744_715_499_319_066_624,
+      abi.encodePacked(),
+      createInitData(),
+      SALT_NONCE
+    );
+
+    vm.stopBroadcast();
+
+    console2.log("Deployed instance at:", instance);
+  }
+}
 
 contract DeployImplementation is Script {
   MultiClaimsHatter public implementation;
@@ -10,7 +56,7 @@ contract DeployImplementation is Script {
 
   // default values
   bool internal _verbose = true;
-  string internal _version = "0.1.0"; // increment this with each new deployment
+  string internal _version = "0.2.0"; // increment this with each new deployment
 
   /// @dev Override default values, if desired
   function prepare(bool verbose, string memory version) public {
@@ -72,7 +118,7 @@ contract DeployPrecompiled is DeployImplementation {
 /* FORGE CLI COMMANDS
 
 ## A. Simulate the deployment locally
-forge script script/MultiClaimsHatter.s.sol -f mainnet
+forge script script/MultiClaimsHatter.s.sol:DeployImplementation -f mainnet
 
 ## B. Deploy to real network and verify on etherscan
 forge script script/MultiClaimsHatter.s.sol -f mainnet --broadcast --verify
